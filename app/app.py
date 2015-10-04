bdd6c6b61476bea7446d3e1b2acb58fa7265d63d
Aaron Dershem <ajdershem@gmail.com>
2015-10-04 02:09:51 -0400

    Adds working ISS fly over functionality
    - Added javascript to get users location
    - Added api end point
    - Display info on page

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 from flask_googlemaps import GoogleMaps
 
@@ -15,6 +15,23 @@ def index():
     iss_lat, iss_lon = get_iss_location()
     return render_template("index.html", people=people,
                            iss_lat=iss_lat, iss_lon=iss_lon)
+
+#    $.getJSON('http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3&alt=20&n=5&callback=?', function(data) {
+#            data['response'].forEach(function (d) {
+#                        var date = new Date(d['risetime']*1000);
+#                                 $('#isspass').append('<li>' + date.toString() + '</li>');
+#                                     });
+#            });
+
+
+@app.route('/api/v1/issposition/<lat>/<lon>')
+def get_iss_pass_time(lat, lon):
+    base_url = 'http://api.open-notify.org/iss-pass.json?'
+    url = base_url + "lat=" + lat + "&lon=" + lon #+ "&alt=20&n=5&callback=?"
+    #url = 'http://api.open-notify.org/astros.json'
+    response = requests.get(url)
+    response = response.json()
+    return jsonify(response)
 
 
 def get_iss_location():
